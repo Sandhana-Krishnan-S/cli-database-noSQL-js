592044d69bfd17d3767b6a63f5993ce3bafc29d5
Sandhana-Krishnan-S <sandhanakrishnansakthivel@gmail.com>
2025-01-27 15:08:01 +0530

    reWrote the write function - append new document added

--- Experiments/createFileTest.js
+++ Experiments/createFileTest.js
@@ -6,7 +6,7 @@ const dataType = ['string' , 'number' , 'boolean']
 const fileName  = 'data.json'
 
 const start = () => {
-    fs.writeFile(path.join(__dirname , "Data", fileName) , "" , (err) => {
+    fs.writeFile(path.join(__dirname , "Data", fileName) , "[]" , (err) => {
         if(err) {
             console.log("error")
         } else {
@@ -15,22 +15,31 @@ const start = () => {
     })
 }
 
-//TODO:: !!read and then right to append datamodel
-const create = (obj) => {
+//TODO: !!read and then right to append datamodel --DONE😊
+//IMPORTANT: !!Always use Try-Catch to call This function 
+const create = async (obj) => {
     if(obj.length > 1) {return new Error('Invalid declaratin format')}
     const name = Object.keys(obj)
     Object.entries(obj[name]).forEach(([key , value]) => {
         if(!dataType.includes(value)) {
-            return new Error('data type mentioned is invalid')
+            throw new Error('data type mentioned is invalid')
         }
-        fs.writeFile(path.join(__dirname , 'Data' , fileName) , JSON.stringify(obj, null, 2), (err) => {
+    })
+    try {
+        const Documents = JSON.parse(fs.readFileSync(path.join(__dirname , 'Data' , fileName) , 'utf8'))
+        console.log(typeof(Documents))
+        console.log(Array.isArray(Documents))
+        Documents.push(obj)
+        fs.writeFile(path.join(__dirname , 'Data' , fileName) , JSON.stringify(Documents, null, 2) , (err) => {
             if(err) {
                 console.log("error")
             } else {
                 console.log("yah!!!")
             }
         })
-    })
+    } catch(err) {
+        throw new Error('Error Reading The document : ' + err.message)
+    }
 }
 
 const addData = () => {
@@ -49,9 +58,9 @@ const deleteFile = () => {
 
 }
 
-start()
+// start()
 
-create({'user1': {
+create({'user': {
     uid: "string",
     name: "string",
     age: "number",
